698667d4c99218fddf83351c14c47bebfc879123
Adwithya <avmagow@gmail.com>
2019-10-20 11:31:12 +0800

    Made Models for Product Backlog, PBI, Project

--- backtrack/backtrack/admin.py
+++ backtrack/backtrack/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import PBI,ProductBacklog,Project
+
 # Register your models here.
+admin.site.register(PBI)
+admin.site.register(Project)
+admin.site.register(ProductBacklog)
--- backtrack/backtrack/models.py
+++ backtrack/backtrack/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductBacklog(models.Model):
+    PBI = models.ForeignKey('PBI', on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['Project', 'PBI'], name="project_PBI")
+        ]
+
+
+class PBI(models.Model):
+    status = models.CharField(max_length=1,
+                              choices=[("N", "Not Done"), ("P", "In Progress"), ("D", "Done")], default="N")
+    story_points = models.PositiveSmallIntegerField()
+    effort_hours = models.PositiveSmallIntegerField()
+
+    class Meta:
+        db_table = "PBI"
+        verbose_name = 'PBI'
+        verbose_name_plural = 'PBIs'
+
+
+class Project(models.Model):
+    pass
